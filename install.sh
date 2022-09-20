23734939bf8fae6da3b9db666bc90721656708ab
Thulasiraman Ilangovan <106608748+datacore-tilangovan@users.noreply.github.com>
2022-09-21 00:12:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update -y 
-apt-get purge kubeadm kubelet kubectl
-apt remove kubeadm kubelet kubectl
+apt purge kubeadm kubelet kubectl -y
+apt remove kubeadm kubelet kubectl -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt-get install kubeadm=1.23.0-00 kubelet=1.23.0-00 kubectl=1.23.0-00 -y
